34e3cb22b14dc25e4388addd3e1d12e6e477f1c4
nelzyzz <sunnel893@gmail.com>
2024-11-11 08:45:16 +0800

    Update config.js

--- config.js
+++ config.js
@@ -1,8 +1,8 @@
 // config.js
 module.exports = {
   // Facebook App Credentials
-  appSecret: 'your_app_secret',
-  accessToken: 'your_page_access_token',
+  appSecret: '4e2a448bb5e5a9494670d34b07213a8e',
+  accessToken: 'EAAZAla0AZCCdUBO9oagE7xRRICf3E1KQ2GmZCfKZAFb1tOb7MZBfFw21EYcbZBH36i5JTX0oeUcn6ruZAGjtqyeZA0dAMyhln7WHK78KZBd1cbqqtV8XEIDD8xZAQTCpr03hDa8UOv3R2506ACC3tbvfr4QDHFmQkakfrEof50MjcZAxaLEHwWTIJ3vVggrwb0aY36kuQZDZD',
   verifyToken: 'pagebot',
 
   // Server Settings
